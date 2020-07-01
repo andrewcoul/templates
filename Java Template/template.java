d81804dce5a2d119aebc5ea16bf12b3f2f6f267c
Andrew Coulson <36116977+andrewcoul@users.noreply.github.com>
2020-07-01 17:26:05 -0500

    Add files via upload
    Added template for Java.

---
+++ Java Template/template.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+import java.io.*;
+import java.text.*;
+import java.math.*;
+import static java.lang.System.*;
+import static java.lang.Integer.*;
+import static java.lang.Double.*;
+import static java.lang.Math.*;
+
+			//change the class name
+public class template
+{
+	public int run() throws Exception
+	{
+		return 400;
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+			//change this to whatever your class name is
+		System.out.println(new template().run());
+	}	
+	
+}
